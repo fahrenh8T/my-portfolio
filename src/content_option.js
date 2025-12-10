9e637e995de424f5301638b1b2756d78fde02265
fahrenh8T <raymondrayn98@gmail.com>
2025-12-10 15:06:11 +0300

    working code

---
+++ src/content_option.js
@@ -0,0 +1,178 @@
+const logotext = "RAYMOND WAIREGI";
+const meta = {
+    title: "Raymond Wairegi",
+    description: "I’m Raymond Wairegi Creative Designer _ Graphic Designer and Frontend Developer,currently working in Kenya",
+};
+
+const introdata = {
+    title: "I’m Rayy ",
+    animated: {
+        first: "Crafted with purpose. Made for you.",
+        second: "Less talking, more creating.",
+        third: " Let’s begin.",
+    },
+    description: "I help businesses and brands turn concepts into products that users love, trust, and keep coming back. Beautiful design and seamless usability.",
+    your_img_url: "https://images.unsplash.com/photo-1761788394380-a38a92897c39?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+};
+
+const dataabout = {
+    title: "abit about myself",
+    aboutme: "A Creative Designer who has crafted a space where imagined ideas come to life.",
+};
+const worktimeline = [{
+        jobtitle: "Graphic Designer",
+        where: "Olive Tree Media",
+        date: "2023",
+    },
+    {
+        jobtitle: "Interior Designer",
+        where: "Caykal Interiors",
+        date: "2022",
+    },
+    {
+        jobtitle: "Intern Graphic Designer",
+        where: "ADN Cottage Industries",
+        date: "2020",
+    },
+];
+
+const skills = [{
+        name: "Adobe Illstrator | Adobe Photoshop",
+        value: 90,
+    },
+    {
+        name: "Figma/Adobe XD",
+        value: 85,
+    },
+    {
+        name: "Davinci Resolve/Adobe Premiere",
+        value: 80,
+    },
+    {
+        name: "Motion Graphics/ After Effects",
+        value: 79,
+    },
+    {
+        name: "HTML/CSS",
+        value: 90,
+    },
+    {
+        name: "React js/ Next js",
+        value: 75,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Crafting intuitive interfaces and seamless experiences that engage users and drive results.",
+    },
+    {
+        title: "Print & Digital Design",
+        description: "Designing brochures, posters, social media graphics, and digital assets that capture attention.",
+    },
+    {
+        title: "Motion Graphics & Animation",
+        description: "Bringing ideas to life with dynamic visuals and engaging motion storytelling.",
+    },
+    {
+        title: "Illustration & Icon Design",
+        description: "Creating custom illustrations and icons that enhance your brand’s personality and communication.",
+    },
+    {
+        title: "Interactive Web Applications",
+        description: "Turning designs into fully functional, visually accurate front-end interfaces.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://www.behance.net/gallery/220024059/Prcieux-Logo",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "raymondrayy12@gmail.com",
+    YOUR_FONE: "(254) 717 837 657",
+    description: "Want to see how thoughtful design and seamless development can elevate your brand? Reach out today, and let’s make it happen.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/fahrenh8T",
+    linkedin: "https://www.linkedin.com/in/raymond-wairegi-a53b1b241/",
+    behance: "https://www.behance.net/raymondrayn",
+    
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
